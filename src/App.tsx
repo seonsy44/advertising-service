291962fc977fdeb6aaf93318dd32613f30863c46
seon <seonsy44@gmail.com>
2022-11-05 17:55:16 +0900

    feat: 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 function App() {
-  return <div />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<div>dashboard</div>} />
+        <Route path="/management" element={<div>management</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
